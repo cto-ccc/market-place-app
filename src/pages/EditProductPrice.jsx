326b7c1c66fe333598ffb5ac67fe350529dfd8f7
unpairedkrish-7 <krishnabhukya893@gmail.com>
2024-01-11 20:43:50 +0530

    Edit data fetch

--- src/pages/EditProductPrice.jsx
+++ src/pages/EditProductPrice.jsx
@@ -1,13 +1,13 @@
 import React, { useEffect, useState } from 'react';
-import { Box , TextField } from '@mui/material';
+import { Box , Button, TextField } from '@mui/material';
 import {useForm } from "react-hook-form"
 
 const EditProductPrice = () => {
-  const [productDetails, setProductDetails] = useState(0);
+  const [productDetails, setProductDetails] = useState([]);
   const {handleSubmit : updateProductSubmit , formState : {errors : productError} , register : registerProducts} = useForm()
 
   useEffect( async()=>{
-    const newPrice = await fetch(`https://api.countrychickenco.in/v2/getLanding` , {
+    const newDetails = await fetch(`https://api.countrychickenco.in/v2/getLanding` , {
       method : "POST",
       headers : {
         "Content-Type" : "application/json",
@@ -19,11 +19,11 @@ const EditProductPrice = () => {
         "userMobile" : null
       }})
     })
-    const data = await newPrice.json();
-    setProductDetails(data)
+    const data = await newDetails.json();
+    setProductDetails(data.productsData)
   },[])
 
-  console.log(productDetails);
+  
 
 
 const onUpdateSubmit  = ()=>{
@@ -31,57 +31,64 @@ const onUpdateSubmit  = ()=>{
 }
 
   return (
-      <form onSubmit={updateProductSubmit(onUpdateSubmit)} style={{ maxWidth : "500px" , margin: "auto"}}>
-          <Box mt={"70px"}  flex flexDirection={"column"}>
-          <Box sx={{fontSize:'20px', pb:1, mb:2, borderBottom:'1px solid #eaeaea'}}>
-            Product Details
-          </Box>
-            <Box mb={3}>
-              <TextField
-                label="Title"
-                variant="outlined"
-                
-                autoFocus
-                autoComplete='off'
-                name="title"
-                value={productDetails.title}
-                {...registerProducts("title", {
-                  required: "Required field"
-                })}
-                error={Boolean(productError?.title)}
-                helperText={productError?.title?.message}
-              />
-              <TextField
-                label="MRP"
-                variant="outlined"
-                
-                autoFocus
-                autoComplete='off'
-                name="mrp"
-                value={productDetails.title}
-                {...registerProducts("mrp", {
-                  required: "Required field"
-                })}
-                error={Boolean(productError?.title)}
-                helperText={productError?.title?.message}
-              />
-              <TextField
-                label="Price"
-                variant="outlined"
-                
-                autoFocus
-                autoComplete='off'
-                name="price"
-                value={productDetails.title}
-                {...registerProducts("price", {
-                  required: "Required field"
-                })}
-                error={Boolean(productError?.title)}
-                helperText={productError?.title?.message}
-              />
+    <>
+
+        <form onSubmit={updateProductSubmit(onUpdateSubmit)} style={{ maxWidth : "500px" , margin: "auto"}}>
+            {productDetails.map((eachdetail)=>{
+              return(<Box mt={"80px"}  flex flexDirection={"column"}>
+              <Box sx={{fontSize:'20px', pb:1, mb:2 , borderBottom:'1px solid #eaeaea' , fontWeight : "600"}}>
+                Product Details
+              </Box>
+                <Box mb={3} sx={{display : "flex" , flexDirection : "column" , gap : "10px"}}>
+                  <TextField
+                    label="Title"
+                    variant="outlined"
+                    
+                    autoFocus
+                    autoComplete='off'
+                    name="title"
+                    value={eachdetail.title}
+                    {...registerProducts("title", {
+                      required: "Required field"
+                    })}
+                    error={Boolean(productError?.title)}
+                    helperText={productError?.title?.message}
+                  />
+                  <TextField
+                    label="MRP"
+                    variant="outlined"
+                    type='number'
+                    autoFocus
+                    autoComplete='off'
+                    name="mrp"
+                    value={eachdetail.data.mrp}
+                    {...registerProducts("mrp", {
+                      required: "Required field"
+                    })}
+                    error={Boolean(productError?.mrp)}
+                    helperText={productError?.mrp?.message}
+                  />
+                  <TextField
+                    label="Price"
+                    variant="outlined"
+                    type='number'
+                    autoFocus
+                    autoComplete='off'
+                    name="price"
+                    value={eachdetail.data.price}
+                    {...registerProducts("price", {
+                      required: "Required field"
+                    })}
+                    error={Boolean(productError?.price)}
+                    helperText={productError?.price?.message}
+                  />
+                </Box>
+                <Button variant='contained' >Update</Button>
+          
             </Box>
-          </Box>  
-      </form>
+            )})}
+        </form>
+    </>
   );
 };
 
